4eefee1f8217e3ae97c8b9631b79f8f3a99d8af7
Ellen Barbara Padilha <ellen.padilha@smn.domain>
2018-09-18 13:33:24 -0300

    Script Lojas de doces SMN

--- ellen/script_lojasmn.sql
+++ ellen/script_lojasmn.sql
@@ -1,19 +1,26 @@
+
 create table cliente 
 	(id smallint primary key identity(100,1),
 	nome varchar(50) not null, 
 	email varchar(100) not null, 
 	cpf numeric (11) not null unique);
 
+create table telefone
+	(id smallint primary key identity(1,1),
+	nemuro numeric(11) not null,
+	idcliente smallint not null,
+	constraint fk_telefone_cliente foreign key (idcliente) references cliente(id));
+
 create table marca
-	(id smallint primary key identity(2,2),
+	(id smallint primary key identity(1,1),
 	nome varchar(50) not null);
 
 create table categoria
-	(id smallint primary key identity(3,2),
-	categoria_produto varchar(50) not null);
-	
+	(id smallint primary key identity(1,1),
+	categoriaproduto varchar(50) not null);
+
 create table venda
-	(id smallint primary key identity (10,2),
+	(id smallint primary key identity(1,1),
 	dtvenda date not null,
 	idcliente smallint not null,
 	constraint fk_venda_cliente foreign key (idcliente) references cliente(id));
@@ -24,7 +31,7 @@ create table itemvenda
 	precovenda money not null,
 	idvenda smallint not null,
 	constraint fk_itemvenda_venda foreign key (idvenda) references venda(id));
-
+	
 create table produto
 	(id smallint primary key identity,
 	nome varchar(50) not null,
@@ -38,10 +45,33 @@ create table produto
 	constraint fk_produto_itemvenda foreign key (iditemvenda) references itemvenda(id),
 	 );
 
+create table itemaquisicao
+	(id smallint primary key identity (1,1),
+	qndeitemadquirido bigint not null,
+	precoaquisicao money not null,
+	idproduto smallint not null,
+	constraint fk_itemaquisicao_produto foreign key(idproduto) references produto(id));
 
-drop table produto
---create table itemaquisicao
---	(id smallint primary key identity (1,1),
---	qndeitemaquisicao bigint not null,
---	precoaquisicao money not null
---	constraint fk_aquisicao_itemaquisicao foreign key(id) references aquisicao(id))
+create table pgto
+	(id smallint primary key identity(1,1),
+	dtpgto date not null,
+	pago bit not null,
+	idvenda smallint not null,
+	idcliente smallint not null, 
+	constraint fk_pgto_venda foreign key (idvenda) references venda(id),
+	constraint fk_pgto_cliente foreign key (idcliente) references cliente(id));
+
+
+	create table tipopgto
+	(id smallint primary key identity (1,1),
+	tipo varchar(50) not null,
+	idpgto smallint
+	constraint fk_tipopgto_pgto foreign key (idpgto) references pgto (id));
+
+	
+
+
+
+
+
+
