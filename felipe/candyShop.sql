1d1e27b6830c0022a043653826004f6027229c0e
Felipe Martins dos Santos <felipe-gf@hotmail.com>
2018-09-18 08:56:23 -0300

    Correção de Erros e adição de campo dtVencimento em pagamentos

--- felipe/candyShop.sql
+++ felipe/candyShop.sql
@@ -19,29 +19,29 @@ create table Administracao.empresa(
 	bairro varchar(75) not null,
 	cep int not null,
 	municipio varchar(75) not null,
-	estado varchar(2) not null constraint chk_estado_empresa check ()
+	estado varchar(2) not null
 )
 go
 
 create table Pessoa.funcionario(
 	id int constraint pk_funcionario primary key identity(1,1),
 	nome varchar(100) not null,
-	dtNasc date not null constraint ck_dtNasc check(getdate() > dtNasc)
+	dtNasc date not null constraint ck_dtNasc check(getdate() > dtNasc),
 	cpf bigint not null constraint un_cpf_funcionario unique,
-	dataCadastro date not null constraint df_dataCadastro_funcionario default(getdate())
+	dataCadastro date not null constraint df_dataCadastro_funcionario default(getdate()),
 	logradouro varchar(100) not null,
 	numero smallint not null,
 	complemento varchar(30),
 	bairro varchar(75) not null,
 	cep int not null,
 	municipio varchar(75) not null,
-	estado varchar(2) not null constraint chk_estado_empresa check (),
+	estado varchar(2) not null,
 	idempresa int constraint fk_funcionario_empresa foreign key references Administracao.empresa(id) not null
 )
 go 
 
 create table Administracao.responsavel(
-	idfuncionario constraint pk_responsavel primary key identity(1,1),
+	idfuncionario int constraint pk_responsavel primary key identity(1,1),
 	dtCadastro date not null constraint df_dtCadastro_responsavel default (getdate()),
 	status date not null,
 	constraint fk_responsavel_funcionario foreign key (idfuncionario) references Pessoa.funcionario(id)
@@ -51,7 +51,7 @@ go
 create table Pessoa.telefone(
 	id int constraint pk_telefone primary key identity(1,1),
 	numero bigint not null constraint un_numero_telefone unique,
-	idfuncionario not null constraint fk_telefone_funcionario foreign key references Pessoa.funcionario(id)
+	idfuncionario int not null constraint fk_telefone_funcionario foreign key references Pessoa.funcionario(id)
 )
 go
 
@@ -65,23 +65,24 @@ go
 create table Transacao.venda(
 	id int constraint pk_venda primary key identity(1,1),
 	idfuncionario int not null constraint fk_venda_funcionario foreign key references Pessoa.funcionario(id),
-	idresponsavel int not null constraint fk_venda_responsavel foreign key references Administracao.responsavel(id),
-	dtVenda not null date constraint df_dtVenda_venda default(getdate())
+	idresponsavel int not null constraint fk_venda_responsavel foreign key references Administracao.responsavel(idfuncionario),
+	dtVenda date not null constraint df_dtVenda_venda default(getdate())
 )
 go
 
 create table Transacao.compra(
 	id int constraint pk_compra primary key identity(1,1),
-	idresponsavel int not null constraint fk_compra_responsavel references Administracao.responsavel(id),
+	idresponsavel int not null constraint fk_compra_responsavel references Administracao.responsavel(idfuncionario),
 	dtCompra date not null constraint df_dtCompra_compra default(getdate())
 )
 go
 
-create table Transcao.pagamento(
+create table Transacao.pagamento(
 	id int constraint pk_pagamento primary key identity(1,1),
 	idvenda int not null constraint fk_pagamento_venda foreign key references Transacao.venda(id),
 	valorPagamento decimal (4,2) constraint chk_valorPagamento_pagamento check(valorPagamento > 0),
-	dtPagamento date not null constraint df_dtPagamento_pagamento defautl(getdate()),
+	dtPagamento date not null constraint df_dtPagamento_pagamento default(getdate()),
+	dtVencimento date not null constraint df_dtVencimento_pagamento default(getdate()),
 	consideracao varchar(75)
 )
 go
