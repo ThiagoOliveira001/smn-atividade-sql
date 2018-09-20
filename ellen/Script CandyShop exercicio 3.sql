6801900f2ab6feeb42cdf53211b30a2cde4881ec
Ellen Barbara Padilha <ellen.padilha@smn.domain>
2018-09-20 15:23:06 -0300

    Coreção exercício 3

--- ellen/Script CandyShop exercicio 3.sql
+++ ellen/Script CandyShop exercicio 3.sql
@@ -1,4 +1,4 @@
 ------03 listar o nome, sexo e status(ativo) de todos os clientes que fazem aniversario em abril
 
-select NomeCliente,Sexo from Cliente 
+select NomeCliente,Sexo, Ativo as 'Status'  from Cliente 
  WHERE  MONTH(Cliente.DataNascimento ) = 03
