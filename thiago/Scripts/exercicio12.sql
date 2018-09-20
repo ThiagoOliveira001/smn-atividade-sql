be1d5702160767b0e18e1003d7ce0fd96c2d3e45
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2018-09-20 08:52:45 -0300

    Script do exercicio 12

---
+++ thiago/Scripts/exercicio12.sql
@@ -0,0 +1,13 @@
+USE LojaDoces;
+GO
+SELECT TOP(10)
+	   p.NomeProduto,
+       m.NomeMarca,
+	   SUM(vi.Quantidade * (p.ValorVenda - p.ValorCompra)) AS Lucro
+FROM Produto AS p
+	INNER JOIN Marca AS m ON m.IdMarca = p.IdMarca
+	INNER JOIN VendaItem AS vi ON vi.IdProduto = p.IdProduto
+	INNER JOIN Venda AS v ON v.IdVenda = vi.IdVenda
+WHERE v.DataPagamento IS NOT NULL
+GROUP BY p.NomeProduto,m.NomeMarca
+ORDER BY Lucro DESC
