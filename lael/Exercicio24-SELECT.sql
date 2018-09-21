31b77e8f2fef961adb01645db8a67776caca13c1
Lael Santos Oliveira <lael_santos@live.com>
2018-09-21 11:41:15 -0300

    Exercicio 24

--- lael/Exercicio24-SELECT.sql
+++ lael/Exercicio24-SELECT.sql
@@ -1,5 +1,5 @@
---listar mês/ano e média mensal de vendas no mês/ano de referencia.
---Média em valor e quantidade
+--listar mês/ano e média mensal de vendas no mês/ano de referencia
+--Média em valor e quantidade.
 
 SELECT MONTH(v.DataVenda) AS Mes, YEAR(v.DataVenda) AS Ano, AVG(VI.Quantidade) AS Quantidade, AVG((P.ValorVenda * VI.Quantidade)) AS Valor
 		FROM Venda AS V
