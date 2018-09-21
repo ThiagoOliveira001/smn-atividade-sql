e107093c47f2b023617978b62d190471001d1b77
Maria Andressa de Paula Silva <maria.silva@smn.domain>
2018-09-21 16:25:36 -0300

    Query Exercicio 24 - Atualização

--- andressa/andressa_query_Exe24.sql
+++ andressa/andressa_query_Exe24.sql
@@ -5,12 +5,14 @@ DATA: 21/09/2018
 */
 
 --Listar mês/ano e média mensal de vendas no mês/ano de referencia. Média em valor e quantidade.
-SELECT DATENAME(MONTH,V.DataVenda) AS Mês, YEAR(V.DataVenda) AS Ano, 
+--SELECT DATENAME(MONTH,V.DataVenda) AS Mês, YEAR(V.DataVenda) AS Ano
+SELECT concat(CAST(DATENAME(MONTH,V.DataVenda)AS VARCHAR(50)),',', YEAR(V.DataVenda)) AS 'Mês/Ano',-- + YEAR(V.DataVenda) AS VARCHAR(50)),
 AVG(VI.Quantidade) AS 'Média Quantidade', CAST(AVG(P.ValorVenda*VI.Quantidade) AS DECIMAL(10,2)) AS 'Média Valor'
 FROM Venda AS V
 INNER JOIN VendaItem AS VI ON VI.IdVenda = V.IdVenda
 INNER JOIN Produto AS P ON P.IdProduto = VI.IdProduto
-GROUP BY DATENAME(MONTH,V.DataVenda), YEAR(V.DataVenda) 
+GROUP BY DATENAME(MONTH,V.DataVenda), YEAR(V.DataVenda)
+ORDER BY YEAR(V.DataVenda)
 ---------------------------------------------------------------------------------------
 
 
