39f388165a4ecc4c9f2810f4f5137bcd233a13ce
iza_mumic <iza95mumic@gmail.com>
2018-09-21 10:39:52 -0300

    Exercicio 24 Banco Loja Doces

---
+++ izabela/script_Exercicio24.sql
@@ -0,0 +1,8 @@
+SELECT FORMAT(v.DataVenda, 'yyyy/MM') AS 'Data', AVG(vi.Quantidade) AS 'Média Quantidade', concat('R$ ', CAST(AVG(p.ValorVenda * vi.Quantidade) AS numeric(15, 2))) AS 'Média Valor'
+FROM VendaItem AS vi
+INNER JOIN Venda AS v
+  ON vi.IdVenda = v.IdVenda
+INNER JOIN Produto AS p
+  ON vi.IdProduto = p.IdProduto
+GROUP BY FORMAT(v.DataVenda, 'yyyy/MM')
+ORDER BY Data DESC
