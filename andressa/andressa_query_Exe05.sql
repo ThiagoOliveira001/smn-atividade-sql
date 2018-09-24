f054c4045f15ac5f2fb87ddc07684875a33469c2
Maria Andressa <andressadipaula@outlook.com>
2018-09-24 14:53:31 -0300

    Query Exercicio 05 - Correção

--- andressa/andressa_query_Exe05.sql
+++ andressa/andressa_query_Exe05.sql
@@ -5,11 +5,37 @@ DATA: 19/09/2018
 */
 
 --Listar o nome do cliente e quantidade de produtos diferentes comprados em 2018
-SELECT P.NomeProduto, C.NomeCliente, VI.Quantidade, V.DataVenda
-FROM Cliente AS C
-INNER JOIN Venda as V ON V.IdCliente= C.IdCliente 
-INNER JOIN VendaItem AS VI ON VI.IdVenda = V.IdVenda
-INNER JOIN Produto AS P ON P.IdProduto= vi.IdProduto
-WHERE YEAR(V.DataVenda)=2018
-ORDER BY c.NomeCliente
+SELECT C.NomeCliente, count(ca.IdProduto)  as 'Quantidade de Produtos'
+from Cliente as c
+cross apply(
+	select p1.IdProduto	
+	from VendaItem as vi1
+	inner join Venda as v1 on v1.IdVenda = vi1.IdVenda
+	inner join Cliente as c1 on c1.IdCliente = v1.IdCliente
+	inner join Produto as p1 on p1.IdProduto = vi1.IdProduto
+	where year(v1.DataVenda)=2018 and c1.IdCliente=c.IdCliente
+	group by p1.IdProduto, c1.NomeCliente
+	
+)as CA
+group by C.NomeCliente
+order by c.NomeCliente
 ---------------------------------------------------------------------------------------
+
+--COM SUBCONSULTA
+--Listar o nome do cliente e quantidade de produtos diferentes comprados em 2018
+
+--SELECT CA.NomeCliente, count(CA.IdProduto) as 'Quantidade de Produtos'
+--from
+--(	select p1.IdProduto, c1.NomeCliente
+--	from VendaItem as vi1
+--	inner join Venda as v1 on v1.IdVenda = vi1.IdVenda
+--	inner join Cliente as c1 on c1.IdCliente = v1.IdCliente
+--	inner join Produto as p1 on p1.IdProduto = vi1.IdProduto
+--	where year(v1.DataVenda)=2018
+--	group by p1.IdProduto, c1.NomeCliente
+	
+--)as CA
+--group by CA.NomeCliente
+--order by cA.NomeCliente
+---------------------------------------------------------------------------------------
+
